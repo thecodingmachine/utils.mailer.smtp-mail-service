a49bb6c7604b5921c5fc80e499363a2e1de89ae4
David Négrier <d.negrier@thecodingmachine.com>
2013-08-09 14:05:53 +0200

    Removing BOM from file

--- src/SmtpMailServiceAdmin.php
+++
@@ -1,9 +0,0 @@
-﻿<?php
-use Mouf\MoufManager;
-
-// Controller declaration
-
-MoufManager::getMoufManager()->declareComponent('smtpmailserviceinstall', 'Mouf\\Utils\\Mailer\\Controllers\\SmtpMailServiceInstallController', true);
-MoufManager::getMoufManager()->bindComponents('smtpmailserviceinstall', 'template', 'moufInstallTemplate');
-MoufManager::getMoufManager()->bindComponents('smtpmailserviceinstall', 'content', 'block.content');
-?>
